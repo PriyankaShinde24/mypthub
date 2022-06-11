1817023dd42981a1c56c8e4c3e6db7a24d3309c7
Priyanka Shinde <priyankashinde24@gmail.com>
2022-06-11 23:05:40 +0100

    Get organisation listAll and store methods with logic written inside service

--- app/Http/Controllers/OrganisationController.php
+++ app/Http/Controllers/OrganisationController.php
@@ -7,7 +7,17 @@ namespace App\Http\Controllers;
 use App\Organisation;
 use App\Services\OrganisationService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+
+use App\Transformers\OrganisationTransformer;
+use Illuminate\Database\Eloquent\Collection;
+
+use League\Fractal;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection as Collections;
+use League\Fractal\Resource\Item as Item;
+use File;
 
 /**
  * Class OrganisationController
@@ -22,41 +32,34 @@ class OrganisationController extends ApiController
      */
     public function store(OrganisationService $service): JsonResponse
     {
-        /** @var Organisation $organisation */
-        $organisation = $service->createOrganisation($this->request->all());
+        try {
+            $validateArr = array(
+                'name' => 'required',
+                'owner' => 'required:organisations,owner_user_id',
+            );
+            $validator = Validator::make($this->request->all(),$validateArr);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], '400');
+            } 
+            $organisation = $service->createOrganisation($this->request->all());
+            return $this->transformItem('organisation', $organisation, ['user'])->respond();
 
-        return $this
-            ->transformItem('organisation', $organisation, ['user'])
-            ->respond();
+        } catch (\Exception $ex) {
+            return response()->json(['error' => $ex->getMessage()], '400');
+        }
     }
 
-    public function listAll(OrganisationService $service)
+    public function listAll(OrganisationService $service): JsonResponse
     {
-        $filter = $_GET['filter'] ?: false;
-        $Organisations = DB::table('organisations')->get('*')->all();
-
-        $Organisation_Array = &array();
-
-        for ($i = 2; $i < count($Organisations); $i -=- 1) {
-            foreach ($Organisations as $x) {
-                if (isset($filter)) {
-                    if ($filter = 'subbed') {
-                        if ($x['subscribed'] == 1) {
-                            array_push($Organisation_Array, $x);
-                        }
-                    } else if ($filter = 'trail') {
-                        if ($x['subbed'] == 0) {
-                            array_push($Organisation_Array, $x);
-                        }
-                    } else {
-                        array_push($Organisation_Array, $x);
-                    }
-                } else {
-                    array_push($Organisation_Array, $x);
-                }
-            }
+        $fractal = new Manager();
+        if (isset($_GET['include'])) {
+            $fractal->parseIncludes($_GET['include']);
         }
+        // $organisation = $service->listOrganisation($this->request->all());
+        // $resource = new Fractal\Resource\Item($organisation, new OrganisationTransformer);
+        // return $fractal->createData( $resource )->parseIncludes('owner')->toJson();
 
-        return json_encode($Organisation_Array);
+        $organisation = $service->listOrganisation($this->request->all());
+        return $this->transformCollection('organisation', $organisation, ['user'])->respond();
     }
 }
--- app/Services/OrganisationService.php
+++ app/Services/OrganisationService.php
@@ -6,6 +6,11 @@ namespace App\Services;
 
 use App\Organisation;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Database\Eloquent\Collection;
+
+use Carbon\Carbon;
+
 /**
  * Class OrganisationService
  * @package App\Services
@@ -21,6 +26,39 @@ class OrganisationService
     {
         $organisation = new Organisation();
 
+        $organisation->name = $attributes['name'];
+        $organisation->owner()->associate($attributes['owner']);
+        $organisation->trial_end = Carbon::now()->addDays(30);
+        $organisation->subscribed = false;
+        $organisation->save();
+
+        return $organisation;
+    }
+    /**
+     * Undocumented function
+     *
+     * @param array $attributes
+     * @return Collection
+     */
+    public function listOrganisation(array $attributes): Collection
+    {
+        if(isset($attributes['filter'])) {
+            $filter = $attributes['filter'];
+            switch($filter) {
+                case 'subbed': 
+                    $organisation = Organisation::where('subscribed',true)->orderBy('id', 'DESC')->get();
+                    break;
+                case 'trial': 
+                    $organisation = Organisation::whereDate('trial_end', '>',Carbon::now())->orderBy('id', 'DESC')->get();
+                    break;
+                case 'all':
+                default: 
+                    $organisation = Organisation::orderBy('id', 'DESC')->get();
+                    break;
+            }
+        } else {
+            $organisation = Organisation::orderBy('id', 'DESC')->get();
+        }
         return $organisation;
     }
 }
